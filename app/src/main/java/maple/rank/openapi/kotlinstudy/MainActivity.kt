2578048a5000f20756680b5d95d9dfa7a02b6ad5
heesang <gmltkd8012@gmail.com>
2024-02-19 23:00:21 +0900

    Kotlin 기초문제: 모바일 알람

--- app/src/main/java/maple/rank/openapi/kotlinstudy/MainActivity.kt
+++ app/src/main/java/maple/rank/openapi/kotlinstudy/MainActivity.kt
@@ -2,10 +2,28 @@ package maple.rank.openapi.kotlinstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val morningNotification = 51
+        val eveningNotification = 135
+
+        printNotificationSummary(morningNotification)
+        printNotificationSummary(eveningNotification)
+
     }
+
+    fun printNotificationSummary(numberOfMessage: Int) {
+        numberOfMessage.let {
+            if (it > 99) Log.e("heesang", "Your phone is blowing up! You have 99+ notifications.")
+            else Log.d("heesang", "You have ${it} notifications.")
+        }
+    }
+
+
 }
