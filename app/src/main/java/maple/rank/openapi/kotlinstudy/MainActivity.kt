327553d2a4078877e4a31e2287cbd1e692a564f2
heesang <gmltkd8012@gmail.com>
2024-02-19 22:54:06 +0900

    람다식과 고차함수

--- app/src/main/java/maple/rank/openapi/kotlinstudy/MainActivity.kt
+++ app/src/main/java/maple/rank/openapi/kotlinstudy/MainActivity.kt
@@ -2,10 +2,54 @@ package maple.rank.openapi.kotlinstudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val coins: (Int) -> String = {
+            "$it quarters"
+        }
+
+        val cupcake: (Int) -> String = {
+            "Have a cupcake!"
+        }
+
+        val treatFunction = trickOrTreat(false) {"$it quarters"}
+        val trickFunction = trickOrTreat(true, null)
+
+//        treatFunction()
+//        trickFunction()
+
+        repeat(4) {
+         treatFunction()
+        }
+        trickFunction()
+
+        //Log.e("heesang", "${trickFunction()} ::: ${trick()}" )
+    }
+
+    fun trickOrTreat(isTrick: Boolean, extraTreat: ((Int) -> String)?): () -> Unit {
+        if (isTrick) {
+            return trick
+        } else {
+            if (extraTreat != null) {
+                Log.d("heesang", "${extraTreat(5)}")
+            }
+            return  treat
+        }
+    }
+
+    val trick: () -> Unit = {
+        Log.i("heesang", "No treats!!!")
+    }
+
+    val treat: () -> Unit = {
+        Log.i("heesang", "Have a treat!!!")
     }
 }
